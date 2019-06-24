33cd61ae82fd136df46f5a273168ba9c092ba9d5
Nitin Singh <nitinsinghit@gmail.com>
2019-06-24 14:23:48 -0400

    blog/templates/blog/post_list.html

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# TEMPLATE_DIRS = (       
+#                   os.path.join(BASE_DIR, 'templates'),
+#                   os.path.join(BASE_DIR, 'templates/welcome')
+
+# )
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -55,7 +61,8 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [], 
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
